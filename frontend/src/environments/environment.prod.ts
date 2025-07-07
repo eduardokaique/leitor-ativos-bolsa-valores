f5aeffa2a3b55565f2b655b1584f2a971657675d
Eduardo Kaique de Freitas <eduardokaique.dev@gmail.com>
2025-07-06 21:32:10 -0300

    👷 setup production environment

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://leitor-ativos-bolsa-valores-production.up.railway.app/api'
-};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://leitor-ativos-bolsa-valores-production.up.railway.app/api'
 };
